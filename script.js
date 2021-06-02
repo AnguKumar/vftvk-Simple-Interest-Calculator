3bdd4041f684bf6fb23c5a0c4e6e5981f5fc4225
AnguKumar <51265421+AnguKumar@users.noreply.github.com>
2021-06-02 15:18:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-body {background-color:black;font-family:arial;color:white}
-h1{color:grey;font-family:verdana}
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years= document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if (principal <= 0 ){
+        alert("Enter a positive number");
+        document.getElementById("principal").values="";
+        document.getElementById("principal").focus();
 
+    }
+    //alert("If you deposit " + principal  + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest);
+    document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    
 
-.maindiv{
-  background-color:white;
-  color:black; 
-  width:300px;
-  paddding:20px;
-  border-radius:25px;
-  align:center
 }
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}      
